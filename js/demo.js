55898050b0f2aa16bdaeba72caeb93fad462aeb8
Adriaan Odendaal <adriaan.odendaal1@gmail.com>
2019-05-13 18:14:38 -0500

    update css & glitch page

---
+++ js/demo.js
@@ -0,0 +1,60 @@
+/**
+ * demo.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2017, Codrops
+ * http://www.codrops.com
+ */
+{
+	setTimeout(() => document.body.classList.add('render'), 30);
+	const navdemos = Array.from(document.querySelectorAll('nav.demos > .demo'));
+	const total = navdemos.length;
+	const current = navdemos.findIndex(el => el.classList.contains('demo--current'));
+	const navigate = (linkEl) => {
+		document.body.classList.remove('render');
+		document.body.addEventListener('transitionend', () => window.location = linkEl.href);
+	};
+	navdemos.forEach(link => link.addEventListener('click', (ev) => {
+		ev.preventDefault();
+		navigate(ev.target);
+	}));
+	document.addEventListener('keydown', (ev) => {
+		const keyCode = ev.keyCode || ev.which;
+		let linkEl;
+		if ( keyCode === 37 ) {
+			linkEl = current > 0 ? navdemos[current-1] : navdemos[total-1];
+		}
+		else if ( keyCode === 39 ) {
+			linkEl = current < total-1 ? navdemos[current+1] : navdemos[0];
+		}
+		else {
+			return false;
+		}
+		navigate(linkEl);
+	});
+	imagesLoaded('.glitch__img', { background: true }, () => {
+		document.body.classList.remove('loading');
+		document.body.classList.add('imgloaded');
+	});
+}
+
+function Helloworld(message){
+	alert(message);
+}
+
+function changeWords(){
+	$('.container').css({'position':'absolute'});
+	setTimeout(function(){	
+		$('.container').css({'position':'relative'});
+	}, 50);
+}
+
+$( document ).ready(function() {
+	setTimeout(Helloworld('Hello world!'), 2000);
+	setTimeout(Helloworld('...I have been watching you...'), 2000);
+	setInterval(changeWords, 5000);
+
+});
