b7c51cb588c433ef43ac84e80123397a0d7004f4
Adriaan Odendaal <adriaan.odendaal1@gmail.com>
2019-05-19 17:57:03 -0500

    update

--- js/demo.js
+++ js/demo.js
@@ -1,12 +1,8 @@
 /**
- * demo.js
+ * Code used from Codrops
  * http://www.codrops.com
- *
- * Licensed under the MIT license.
+ * Under under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2017, Codrops
- * http://www.codrops.com
  */
 {
 	setTimeout(() => document.body.classList.add('render'), 30);
@@ -46,15 +42,139 @@ function Helloworld(message){
 }
 
 function changeWords(){
-	$('.container').css({'position':'absolute'});
+	$('#text_glitch').css({'display':'block'});
+	$('.glitch__imglogo').css({'height':'50%'});
 	setTimeout(function(){	
-		$('.container').css({'position':'relative'});
-	}, 50);
+		$('.glitch__imglogo').css({'display':'none'});
+	}, 150);
+	setTimeout(function(){	
+		$('#text_glitch').css({'display':'none'});
+		$('.glitch__imglogo').css({'display':'block'});
+		$('.glitch__imglogo').css({'height':'calc(100% + var(--gap-vertical) * 2)'});
+
+	}, 1000);
 }
 
 $( document ).ready(function() {
-	setTimeout(Helloworld('Hello world!'), 2000);
-	setTimeout(Helloworld('...I have been watching you...'), 2000);
+	//setTimeout(Helloworld('Hello world!'), 2000);
+	//setTimeout(Helloworld('...I have been watching you...'), 2000);
 	setInterval(changeWords, 5000);
 
 });
+
+
+//Text glitch from Ivan http://ivandaum.fr/
+//https://codepen.io/ivandaum/pen/WRxRwv
+
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function getRandomLetter() {
+ var alphabet = ['<','!','%','>','/','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+ return alphabet[rand(0,alphabet.length - 1)]
+}
+function getRandomWord(word) {
+  var text = word.innerHTML
+  
+  var finalWord = ''
+  for(var i=0;i<text.length;i++) {
+    finalWord += text[i] == ' ' ? ' ' : getRandomLetter()
+  }
+ 
+  return finalWord
+}
+
+var word = document.querySelector('#coming_soon')
+var interv = 'undefined'
+var canChange = false
+var globalCount = 0
+var count = 0
+var INITIAL_WORD = word.innerHTML;
+var isGoing = false
+
+function init() {
+ if(isGoing) return;
+ 
+ isGoing = true
+ var randomWord = getRandomWord(word)
+ word.innerHTML = randomWord
+
+ interv = setInterval(function() {
+  var finalWord = ''
+  for(var x=0;x<INITIAL_WORD.length;x++) {
+   if(x <= count && canChange) {
+    finalWord += INITIAL_WORD[x]
+   } else {
+    finalWord += getRandomLetter()
+   }
+  }
+  word.innerHTML = finalWord
+  if(canChange) {
+    count++
+  }
+  if(globalCount >= 20) {
+   canChange = true
+  }
+  if(count>=INITIAL_WORD.length) {
+   clearInterval(interv)
+   count = 0
+   canChange = false
+   globalCount = 0
+   isGoing = false
+  }
+  globalCount++
+ },50)
+ 
+}
+
+
+
+word.addEventListener('mouseenter', init)
+
+//Text hacker 
+
+var intervalID = window.setInterval(updateScreen, 200);
+var c = document.getElementById("text_glitch");
+
+var txt = [
+  "FORCE: XX0022. ENCYPT://000.222.2345",
+  "TRYPASS: ********* AUTH CODE: ALPHA GAMMA: 1___ PRIORITY 1",
+  "RETRY: REINDEER FLOTILLA",
+  "Z:> /FALKEN/GAMES/TICTACTOE/ EXECUTE -PLAYERS 0",
+  "================================================",
+  "Priority 1 // local / scanning...",
+  "scanning ports...",
+  "BACKDOOR FOUND (23.45.23.12.00000000)",
+  "BACKDOOR FOUND (13.66.23.12.00110000)",
+  "BACKDOOR FOUND (13.66.23.12.00110044)",
+  "...",
+  "...",
+  "BRUTE.EXE -r -z",
+  "...locating vulnerabilities...",
+  "...vulnerabilities found...",
+  "MCP/> DEPLOY CLU",
+  "SCAN: __ 0100.0000.0554.0080",
+  "SCAN: __ 0020.0000.0553.0080",
+  "SCAN: __ 0001.0000.0554.0550",
+  "SCAN: __ 0012.0000.0553.0030",
+  "SCAN: __ 0100.0000.0554.0080",
+  "SCAN: __ 0020.0000.0553.0080",
+]
+
+var docfrag = document.createDocumentFragment();
+
+function updateScreen() {
+  //Shuffle the "txt" array
+  txt.push(txt.shift());
+  //Rebuild document fragment
+  txt.forEach(function(e) {
+    var p = document.createElement("p");
+    p.textContent = e;
+    docfrag.appendChild(p);
+  });
+  //Clear DOM body
+  while (c.firstChild) {
+    c.removeChild(c.firstChild);
+  }
+  c.appendChild(docfrag);
+}
