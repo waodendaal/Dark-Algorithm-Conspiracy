ae734c2b96ccc32a9dad9cd14be8b9c44431a8f9
Adriaan Odendaal <adriaan.odendaal1@gmail.com>
2019-07-03 15:31:13 -0500

    modal fix

--- js/glitch.js
+++ js/glitch.js
@@ -190,16 +190,70 @@ var span = document.getElementsByClassName("close")[0];
 
 $('#coming_soon').on('click', function(){
   modal.style.display = "block";
+  console.log($('.background_image').html())
+  $('.background_image').html('<div class="glitch">\
+                                  <div class="glitch__img"></div>\
+                                </div>');
 })
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  $('.background_image').html('<div class="glitch">\
+                                  <div class="glitch__img"></div>\
+                                  <div class="glitch__img"></div>\
+                                  <div class="glitch__img"></div>\
+                                  <div class="glitch__img"></div>\
+                                  <div class="glitch__img"></div>\
+                                </div>');
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
       modal.style.display = "none";
+  }
+}
+
+dragElement(document.getElementById("myModal"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
   }
 }
